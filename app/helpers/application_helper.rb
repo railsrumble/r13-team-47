fe987a353323cddaa6280351bd5f4d8923c319bc
Silvano Stralla <silvano.stralla@sistrall.it>
2013-10-19 22:57:22 +0200

    Form design and Markdown support for team description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+  def markdown(text)
+    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true)
+    @markdown.render(text)
+  end
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
