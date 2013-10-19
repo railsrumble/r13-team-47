d4a260b98e066a1a0d0b5ccb9ff7ff248fb36dd4
Silvano Stralla <silvano.stralla@sistrall.it>
2013-10-19 20:14:41 +0200

    Basic team form

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,3 +1,11 @@
 class TeamsController < ApplicationController
   inherit_resources
+
+  def resource_params
+    if request.get?
+      [] 
+    else
+      [params.require(:team).permit(:url, :name, :description)]
+    end
+  end
 end
