f0bc6f4de95d0d21fe8df6f4758e102e0a468a68
Silvano Stralla <silvano.stralla@sistrall.it>
2013-10-19 19:50:52 +0200

    Model work

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,8 @@
 class Team < ActiveRecord::Base
   has_many :works
   has_many :sites, through: :works
+
+  def self.random
+    order("RAND()").first
+  end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,6 +1,4 @@
 class Work < ActiveRecord::Base
   belongs_to :team
   belongs_to :site
-
-  has_and_belongs_to_many :users
 end
