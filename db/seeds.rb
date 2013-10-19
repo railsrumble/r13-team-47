83583fc4e252d81e470cfb392dead6717dc9d356
Fabrizio Monti <fabrizio.monti@welaika.com>
2013-10-19 17:59:31 +0200

    add team seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+Team.create([
+  {
+    name: 'weLaika',
+    url: 'http://dev.welaika.com',
+    level: 10,
+    score: 567,
+    description: Faker::Lorem::sentences(10).join(' ')
+  },
+  {
+    name: 'New Team',
+    url: 'http://newteam.example.com',
+    level: 56,
+    score: 593835,
+    description: Faker::Lorem::sentences(20).join(' ')
+  },
+  {
+    name: 'Muppet',
+    url: 'http://muppet.example.com',
+    level: 7,
+    score: 23,
+    description: Faker::Lorem::sentences(5).join(' ')
+  }
+])
