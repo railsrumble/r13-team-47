fd0a48fa52dcbadeb1b8d068f0d43669a2be4594
Silvano Stralla <silvano.stralla@sistrall.it>
2013-10-19 09:32:39 +0200

    App setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PinkTeam::Application.config.secret_key_base = '2c5650a34489b8f400180505f38cfec1c5380ab7751395ccdc6cbf2104c38b43c3db7035e4197cbc95fdfa41d4b51c5490f270c3583103a0501d164ec846726e'
