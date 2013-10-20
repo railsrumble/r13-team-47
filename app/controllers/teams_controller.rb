6d4b123bf241de9b90eb7122e7ff2d11a28a9bf8
Silvano Stralla <silvano.stralla@sistrall.it>
2013-10-20 09:33:38 +0200

    Debug and refactoring

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,4 +1,8 @@
 class TeamsController < WebWeightController
+  def index
+    @teams = Team.all
+  end
+
   def new
     @team = current_user.teams.new
   end
@@ -8,7 +12,17 @@ class TeamsController < WebWeightController
       team.users << current_user
     end
 
-    redirect_to teams_url(@team)
+    redirect_to team_url(@team)
+  end
+
+  def edit
+    @team = current_user.teams.first
+  end
+
+  def update
+    @team = Team.find(params[:id]).update(permitted_params[:team])
+
+    redirect_to team_url(@team)
   end
 
   def show
