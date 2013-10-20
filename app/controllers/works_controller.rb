34acf196413e5e7054a33630d6f4be527dbc3ef7
Fabrizio Monti <fabrizio.monti@welaika.com>
2013-10-20 21:42:06 +0200

    cannot update team points from there

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,7 +2,6 @@ class WorksController < ApplicationController
   def create
     @team = Team.find(params[:team_id])
     CreateWork.run(@team, params)
-    TeamRankUpdater.run(@team)
 
     redirect_to edit_team_url(@team)
   end
@@ -10,7 +9,6 @@ class WorksController < ApplicationController
   def destroy
     @team = Team.find(params[:team_id])
     @team.works.find(params[:id]).destroy
-    TeamRankUpdater.run(@team)
 
     redirect_to team_url(@team)
   end
