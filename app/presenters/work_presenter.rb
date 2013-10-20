5ce4d30e72b16d5eb2a3f84520e34439eabaa382
Fabrizio Monti <fabrizio.monti@welaika.com>
2013-10-20 18:42:00 +0200

    fix work url and link button

--- app/presenters/work_presenter.rb
+++ app/presenters/work_presenter.rb
@@ -4,4 +4,12 @@ class WorkPresenter < Showcase::Presenter
     h.image_tag object.screenshot.thumb, class: "site-thumb"
   end
 
+  def host
+    object.url.sub(/^https?\:\/\//, '') if object.url
+  end
+
+  def formatted_points
+    "#{h.number_with_delimiter(object.points)} points"
+  end
+
 end
