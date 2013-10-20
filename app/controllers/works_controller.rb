f48de992efcfe7e0cd2dc9f950e50be53c16e3cb
Silvano Stralla <silvano.stralla@sistrall.it>
2013-10-20 12:00:53 +0200

    Support to deleting works

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -6,6 +6,13 @@ class WorksController < ApplicationController
     redirect_to team_url(@team)
   end
 
+  def destroy
+    @team = Team.find(params[:team_id])
+    @team.works.find(params[:id]).destroy
+
+    redirect_to team_url(@team)
+  end
+
   protected
     def  permitted_params
       params.permit(work: [:url, :name])
--- app/models/work.rb
+++ app/models/work.rb
@@ -3,4 +3,6 @@ class Work < ActiveRecord::Base
 
   belongs_to :team
   belongs_to :site
+
+  delegate :url?, to: 'site', allow_nil: true
 end
