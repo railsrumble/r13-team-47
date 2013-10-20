abc23803997139e2d865c20015d8eb827efb8079
Filippo Gangi Dino <filippo.gangidino@welaika.com>
2013-10-21 00:30:29 +0200

    nav bar fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,8 +10,12 @@ module ApplicationHelper
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  def is_active(controller, action)       
-    params[:controller] == controller && params[:action] == action ? "active" : nil        
+  def is_active(controller, action, id = nil)
+    if id 
+      params[:controller] == controller && params[:action] == action && params[:id] == id ? "active" : nil
+    else
+      params[:controller] == controller && params[:action] == action ? "active" : nil    
+    end  
   end
 
 end
